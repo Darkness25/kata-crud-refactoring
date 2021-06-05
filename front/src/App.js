755a1d49a5743b6f9a9fa1142e22ece0bf72012c
JesusL <thedarkness2555@gmail.com>
2021-06-04 23:50:02 -0500

    Se agregan los componentes para la nueva funcionalidad de la lista de todos, y se agrega diseño al front

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Form from './component/Form';
 import List from './component/List';
+import MayorForm from './component/MayorForm';
 import { StoreProvider } from './component/Store';
-
 
 function App() {
   return <StoreProvider>
+    <div className= 'container'>
     <h3>To-Do List</h3>
     <Form />
-    <List />
+    <List /> 
+    <h3>Master List</h3>
+    <MayorForm />
+    </div>
   </StoreProvider>
 }
 
